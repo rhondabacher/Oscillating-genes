04ee698cbfd6ce169b7fe5e0928f94b0f83b0826
Rhonda Bacher <rhonda.bacher@gmail.com>
2017-02-02 17:55:21 -0600

    first commit and push

---
+++ server.R
@@ -0,0 +1,245 @@
+
+library(shiny)
+library(Oscope)
+shinyServer(function(input, output) {
+  
+  # Expression that generates a histogram. The expression is
+  # wrapped in a call to renderPlot to indicate that:
+  #
+  #  1) It is "reactive" and therefore should re-execute automatically
+  #     when inputs change
+  #  2) Its output type is a plot
+  
+  
+  output$distPlot1 <- renderPlot({
+    
+    if(input$subgroups == 1) { 
+    a = seq(0, 1,length.out = input$NumCells+1)[-1]
+    
+    phase =  seq(0, input$phaseShifts * pi, input$phaseShifts * pi/input$NumGenesOSC)[1:input$NumGenesOSC]  #phase = rep(0,100)
+    sinNoisec1 = NULL
+    for(k in 1:length(phase)) {
+
+      sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC * a + phase[k] ) ) + rnorm(input$NumCells, 0, input$varOSC))
+    }
+    for(k in 1:input$NumGenesNOISE) {
+
+      sinNoisec1 = rbind( sinNoisec1, rnorm(input$NumCells, 0, input$varNOISE))
+    }
+    sinNoisec1 <- NormForSine(sinNoisec1)
+    pp <- prcomp(sinNoisec1, center=T, scale=T)
+    par(mar=c(5,5,1,1))
+    col.conds <- c(rep("red", input$NumGenesOSC), rep("black", input$NumGenesNOISE))
+    plot(pp$x[,1], pp$x[,2], xlab="PC1", ylab="PC2", pch=16, cex.lab=1.5, cex.axis=1.5, col=col.conds)
+    box(lwd=2)
+}
+    
+    })
+
+  output$heatCor1 <- renderPlot({
+    if(input$subgroups == 1) { 
+      a = seq(0, 1,length.out = input$NumCells+1)[-1]
+      
+      phase =  seq(0, input$phaseShifts * pi, input$phaseShifts * pi/input$NumGenesOSC)[1:input$NumGenesOSC]  #phase = rep(0,100)
+      sinNoisec1 = NULL
+      for(k in 1:length(phase)) {
+        
+        sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC * a + phase[k] ) ) + rnorm(input$NumCells, 0, input$varOSC))
+      }
+      for(k in 1:input$NumGenesNOISE) {
+        
+        sinNoisec1 = rbind( sinNoisec1, rnorm(input$NumCells, 0, input$varNOISE))
+      }
+      sinNoisec1 <- NormForSine(sinNoisec1)
+    cor.mat <- cor(t(sinNoisec1))
+    heatmap.2(cor.mat,  # same data set for cell labels
+              main = "Gene correlations", # heat map title
+              notecol="black",      # change font color of cell labels to black
+              density.info="none",  # turns off density plot inside color legend
+              trace="none",         # turns off trace lines inside the heat map
+              col=my_palette,       # use on color palette defined earlier
+              # breaks=col_breaks,
+              margins =c(7,7),   # enable color transition at specified limits
+              dendrogram="none",     # only draw a row dendrogram
+              Colv="none", Rowv="none", scale='none')  
+    
+  }
+    })
+  
+  output$scatterGenes1 <- renderPlot({
+    if(input$subgroups == 1) { 
+      a = seq(0, 1,length.out = input$NumCells+1)[-1]
+      
+      phase =  seq(0, input$phaseShifts * pi, input$phaseShifts * pi/input$NumGenesOSC)[1:input$NumGenesOSC]  #phase = rep(0,100)
+      sinNoisec1 = NULL
+      for(k in 1:length(phase)) {
+        
+        sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC * a + phase[k] ) ) + rnorm(input$NumCells, 0, input$varOSC))
+      }
+      for(k in 1:input$NumGenesNOISE) {
+        
+        sinNoisec1 = rbind( sinNoisec1, rnorm(input$NumCells, 0, input$varNOISE))
+      }
+      sinNoisec1 <- NormForSine(sinNoisec1)
+      (plot(sinNoisec1[1,], pch=16, main=paste0("Gene: ",i), xlab="Cell", ylab="Expression"))
+ 
+    }
+  })
+
+
+
+
+  output$distPlot2 <- renderPlot({
+
+    if(input$subgroups == 2) {  
+    a = seq(0, 1,length.out = input$NumCells2+1)[-1]
+
+      phase =  seq(input$phaseShifts1[1] * pi, input$phaseShifts1[2] * pi, length = input$NumGenesOSC1)  #phase = rep(0,100)
+      sinNoisec1 = NULL
+      for(k in 1:length(phase)) {
+
+        sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC1 * a + phase[k] ) ) + rnorm(input$NumCells2, 0, input$varOSC1))
+      }
+      phase =  seq(input$phaseShifts2[1] * pi, input$phaseShifts2[2] * pi, length = input$NumGenesOSC2)  #phase = rep(0,100)
+      for(k in 1:length(phase)) {
+
+        sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC2 * a + phase[k] ) ) + rnorm(input$NumCells2, 0, input$varOSC2))
+      }
+      for(k in 1:input$NumGenesNOISE2) {
+
+        sinNoisec1 = rbind( sinNoisec1, rnorm(input$NumCells2, 0, input$varNOISE2))
+      }
+      sinNoisec1 <- NormForSine(sinNoisec1)
+      pp <- prcomp(sinNoisec1, center=T, scale=T)
+      par(mar=c(5,5,1,1))
+      col.conds <- c(rep("red", input$NumGenesOSC1), rep("blue", input$NumGenesOSC2), rep("black", input$NumGenesNOISE))
+      plot(pp$x[,1], pp$x[,2], xlab="PC1", ylab="PC2", pch=16, cex.lab=1.5, cex.axis=1.5, col=col.conds)
+      box(lwd=2)
+}
+    })
+
+  output$heatCor2 <- renderPlot({
+    if(input$subgroups == 2) {
+      a = seq(0, 1,length.out = input$NumCells2+1)[-1]
+      
+      phase =  seq(input$phaseShifts1[1] * pi, input$phaseShifts1[2] * pi, length = input$NumGenesOSC1)  #phase = rep(0,100)
+      sinNoisec1 = NULL
+      for(k in 1:length(phase)) {
+        
+        sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC1 * a + phase[k] ) ) + rnorm(input$NumCells2, 0, input$varOSC1))
+      }
+      phase =  seq(input$phaseShifts2[1] * pi, input$phaseShifts2[2] * pi, length = input$NumGenesOSC2)  #phase = rep(0,100)
+      for(k in 1:length(phase)) {
+        
+        sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC2 * a + phase[k] ) ) + rnorm(input$NumCells2, 0, input$varOSC2))
+      }
+      for(k in 1:input$NumGenesNOISE2) {
+        
+        sinNoisec1 = rbind( sinNoisec1, rnorm(input$NumCells2, 0, input$varNOISE2))
+      }
+      sinNoisec1 <- NormForSine(sinNoisec1)
+      
+       cor.mat <- cor(t(sinNoisec1))
+    heatmap.2(cor.mat,  # same data set for cell labels
+              main = "Gene correlations", # heat map title
+              notecol="black",      # change font color of cell labels to black
+              density.info="none",  # turns off density plot inside color legend
+              trace="none",         # turns off trace lines inside the heat map
+              col=my_palette,       # use on color palette defined earlier
+              # breaks=col_breaks,
+              margins =c(7,7),   # enable color transition at specified limits
+              dendrogram="none",     # only draw a row dendrogram
+              Colv="none", Rowv="none", scale='none')
+    }
+
+  })
+  
+  output$scatterGenes2 <- renderPlot({
+    if(input$subgroups == 2) {      
+      a = seq(0, 1,length.out = input$NumCells2+1)[-1]
+    
+    phase =  seq(input$phaseShifts1[1] * pi, input$phaseShifts1[2] * pi, length = input$NumGenesOSC1)  #phase = rep(0,100)
+    sinNoisec1 = NULL
+    for(k in 1:length(phase)) {
+      
+      sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC1 * a + phase[k] ) ) + rnorm(input$NumCells2, 0, input$varOSC1))
+    }
+    phase =  seq(input$phaseShifts2[1] * pi, input$phaseShifts2[2] * pi, length = input$NumGenesOSC2)  #phase = rep(0,100)
+    for(k in 1:length(phase)) {
+      
+      sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC2 * a + phase[k] ) ) + rnorm(input$NumCells2, 0, input$varOSC2))
+    }
+    for(k in 1:input$NumGenesNOISE2) {
+      
+      sinNoisec1 = rbind( sinNoisec1, rnorm(input$NumCells2, 0, input$varNOISE2))
+    }
+    sinNoisec1 <- NormForSine(sinNoisec1)
+      (plot(sinNoisec1[1,], pch=16, main=paste0("Gene: ",i), xlab="Cell", ylab="Expression"))
+      
+    }
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  output$ellipsePlot<- renderPlot({
+    a = seq(0, 1,length.out = input$NumCells3+1)[-1]
+    
+    sinNoisec1 = NULL
+
+      sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC3 * a + input$phaseShifts3 ) ) + rnorm(input$NumCells3, 0, input$varOSC3))
+      sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC4 * a + input$phaseShifts4 ) ) + rnorm(input$NumCells3, 0, input$varOSC4))
+      
+    sinNoisec1 <- NormForSine(sinNoisec1)
+    
+    (plot(sinNoisec1[1,],sinNoisec1[2,], pch=16, main="", xlab="Gene 1", ylab="Gene 2"))
+    
+  })
+  
+  
+  
+  
+  ellipseData <- reactive({
+    a = seq(0, 1,length.out = input$NumCells3+1)[-1]
+    
+    sinNoisec1 = NULL
+    
+    sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC3 * a + input$phaseShifts3 ) ) + rnorm(input$NumCells3, 0, input$varOSC3))
+    sinNoisec1 = rbind( sinNoisec1, c( sin(2 * pi * input$freqOSC4 * a + input$phaseShifts4 ) ) + rnorm(input$NumCells3, 0, input$varOSC4))
+    
+    sinNoisec1 <- NormForSine(sinNoisec1)
+    
+    List = sinNoisec1
+  })
+  
+  
+  output$ellipsePlot<- renderPlot({
+    sinNoisec1 = ellipseData()
+    
+    (plot(sinNoisec1[1,], sinNoisec1[2,], pch=16, main="", xlab="Gene 1", ylab="Gene 2"))
+  
+    
+  })
+  
+  
+  output$geneP1<- renderPlot({
+    sinNoisec1 = ellipseData()
+    (plot(sinNoisec1[1,], pch=16, main="Gene 1", xlab="Cell", ylab="Expression"))
+    
+    
+  })
+  
+  
+  output$geneP2<- renderPlot({
+    
+    sinNoisec1 = ellipseData()
+    (plot(sinNoisec1[2,], pch=16, main="Gene 2", xlab="Cell", ylab="Expression"))
+    
+    
+  })
+  
+})
---
+++ ui.R
@@ -0,0 +1,211 @@
+library(shiny)
+library(shinyFiles)
+# options(shiny.maxRequestSize=500*1024^2) 
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  titlePanel("Oscillating genes!"),
+  # Application title
+  headerPanel("What do you want to do?"),
+  
+  checkboxInput("pca", "PCA with oscillating genes ?"),
+  checkboxInput("ellipse", "Compare two oscillating genes?"),
+  
+  conditionalPanel(width=20,height=10,  
+    condition = "input.pca == true",
+    
+    selectInput("subgroups", "How many groups of oscillating genes?",
+              c("",One = "1", Two = "2"),selected = NULL),
+  
+    conditionalPanel(width=20,height=10,  
+    condition = "input.subgroups == 1",            
+      column(width=4,
+        sliderInput("NumCells",
+                    "Number of Cells:",
+                    min = 5,
+                    max = 200,
+                    value = 100),
+
+      sliderInput("NumGenesOSC",
+                  "Number of oscillating genes:",
+                  min = 5,
+                  max = 200,
+                  value = 10),
+
+      sliderInput("NumGenesNOISE",
+                  "Number of noise genes:",
+                  min = 0,
+                  max = 200,
+                  value = 10),
+      # column(width=4,
+      # ## Do you want the phase shifts to go all the way around? 
+      # ## Do you 
+      sliderInput("phaseShifts",
+                  "How far sould the phase shift go ?:",
+                  min = .1,
+                  max = 2,
+                  value = 2,
+                  round=FALSE),
+      # 
+      sliderInput("varOSC",
+                  "How much noise should be in the oscillating genes ?:",
+                  min = 0,
+                  max = 1,
+                  value = .05),
+      sliderInput("varNOISE",
+                  "How much noise should be in the noise genes ?:",
+                  min = 0,
+                  max = 1,
+                  value = .05),
+      sliderInput("freqOSC",
+                  "What should the frequency be?:",
+                  min = .1,
+                  max = 5,
+                  value = 1))
+    ,
+    column(7,
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        tabPanel("PCA plot", plotOutput("distPlot1")),
+        tabPanel("Gene Correlation plot", plotOutput("heatCor1")),
+        tabPanel("Example gene plot", plotOutput("scatterGenes1")))
+    ))),
+    
+    
+    
+    
+    
+    
+conditionalPanel(width=20,height=10,  
+ condition = "input.subgroups == 2",            
+ column(width=4,
+        sliderInput("NumCells2",
+                    "Number of Cells:",
+                    min = 5,
+                    max = 200,
+                    value = 100),
+        
+        sliderInput("NumGenesOSC1",
+                    "Number of oscillating genes in group 1:",
+                    min = 5,
+                    max = 200,
+                    value = 10),
+        sliderInput("NumGenesOSC2",
+                    "Number of oscillating genes in group 2:",
+                    min = 5,
+                    max = 200,
+                    value = 10),
+        sliderInput("NumGenesNOISE2",
+                    "Number of noise genes:",
+                    min = 0,
+                    max = 200,
+                    value = 10),
+
+        sliderInput("phaseShifts1",
+                    "How far sould the phase shift go in group 1?:",
+                    min = .1,
+                    max = 2,
+                    value = c(1,2),
+                    round=FALSE),
+        sliderInput("phaseShifts2",
+                    "How far sould the phase shift go in group 2?:",
+                    min = .1,
+                    max = 2,
+                    value = c(.1,.8),
+                    round=FALSE),
+  
+        sliderInput("varOSC1",
+                    "How much noise should be in the oscillating genes in group 1 ?:",
+                    min = 0,
+                    max = 1,
+                    value = .05),
+        sliderInput("varOSC2",
+                    "How much noise should be in the oscillating genes in group 2 ?:",
+                    min = 0,
+                    max = 1,
+                    value = .05),
+        sliderInput("varNOISE2",
+                    "How much noise should be in the noise genes ?:",
+                    min = 0,
+                    max = 1,
+                    value = .05),
+        sliderInput("freqOSC1",
+                    "What should the frequency be in group 1 ?:",
+                    min = .1,
+                    max = 5,
+                    value = 1),
+        sliderInput("freqOSC2",
+                    "What should the frequency be in group 2 ?:",
+                    min = .1,
+                    max = 5,
+                    value = 1)),
+        column(7,
+               # Show a plot of the generated distribution
+               mainPanel(
+                 tabsetPanel(
+                   tabPanel("PCA plot", plotOutput("distPlot2")),
+                   tabPanel("Gene Correlation plot", plotOutput("heatCor2")),
+                   tabPanel("Gene Scatter plots", plotOutput("scatterGenes2")))
+               ))
+ )),
+
+conditionalPanel(width=20,height=10,  
+                 condition = "input.ellipse == true",
+
+                 column(width=4,
+                        sliderInput("NumCells3",
+                                    "Number of Cells:",
+                                    min = 5,
+                                    max = 200,
+                                    value = 100),
+                        sliderInput("phaseShifts3",
+                                    "Phase shift in gene 1?:",
+                                    min = .1,
+                                    max = 2,
+                                    value = c(1),
+                                    round=FALSE),
+                        sliderInput("phaseShifts4",
+                                    "Phase shift in gene 2?:",
+                                    min = .1,
+                                    max = 2,
+                                    value = c(.8),
+                                    round=FALSE),
+                        
+                        sliderInput("varOSC3",
+                                    "How much noise should be in gene 1 ?:",
+                                    min = 0,
+                                    max = 1,
+                                    value = .05),
+                        sliderInput("varOSC4",
+                                    "How much noise should be in gene 2 ?:",
+                                    min = 0,
+                                    max = 1,
+                                    value = .05),
+                      
+                        sliderInput("freqOSC3",
+                                    "What should the frequency be for gene 1 ?:",
+                                    min = .1,
+                                    max = 5,
+                                    value = 1),
+                        sliderInput("freqOSC4",
+                                    "What should the frequency be for gene 2 ?:",
+                                    min = .1,
+                                    max = 5,
+                                    value = 1)),
+                 
+                 
+
+                 
+                 column(7,
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          tabsetPanel(
+                            tabPanel("Gene vs. Gene plot", plotOutput("ellipsePlot")),
+                            tabPanel("Gene 1 plot", plotOutput("geneP1")),
+                            tabPanel("Gene 2 plot", plotOutput("geneP2")))))
+
+
+)
+))
+  
